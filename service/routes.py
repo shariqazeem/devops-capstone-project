6cdd8fb54a75b938036197479bc6aa7bf379ba67
shariqazeem <101935656+shariqazeem@users.noreply.github.com>
2023-03-30 02:15:39 +0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -59,13 +59,6 @@ def create_accounts():
     )
 
 
-######################################################################
-# LIST ALL ACCOUNTS
-######################################################################
-
-
-
-
 # ... place you code here to LIST accounts ...
     ######################################################################
     # LIST ALL ACCOUNTS
@@ -87,16 +80,6 @@ def list_accounts():
     return jsonify(account_list), status.HTTP_200_OK
 
 
-######################################################################
-# READ AN ACCOUNT
-######################################################################
-
-
-    app.logger.info("Returning [%s] accounts", len(account_list))
-    return jsonify(account_list), status.HTTP_200_OK
-
-
-
     ######################################################################
     # READ AN ACCOUNT
     ######################################################################
@@ -114,13 +97,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
-
-######################################################################
-# UPDATE AN EXISTING ACCOUNT
-######################################################################
-
-
 
 
 # ... place you code here to UPDATE an account ...
@@ -153,6 +129,7 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -171,7 +148,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
 
-=======
+
 # ... place you code here to DELETE an account ...
     ######################################################################
     # DELETE AN ACCOUNT
@@ -190,6 +167,7 @@ def delete_accounts(account_id):
 
 
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
